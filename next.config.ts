305473c9a389fb093f0ef8070159c10027c880c7
Rafa Audibert <rafaeelaudibert@gmail.com>
2025-09-14 18:22:02 -0300

    feat: Add PostHog

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
     images: {
         remotePatterns: [{ hostname: 'bold-jellyfish-927.convex.cloud', protocol: 'https' }],
     },
+    async rewrites() {
+        return [
+            {
+                source: '/ingest/static/:path*',
+                destination: 'https://us-assets.i.posthog.com/static/:path*',
+            },
+            {
+                source: '/ingest/:path*',
+                destination: 'https://us.i.posthog.com/:path*',
+            },
+        ]
+    },
+    skipTrailingSlashRedirect: true,
 }
 
 export default nextConfig
